27d01d01b1e0dc5c588152f36072d0d0b21634f0
pwk <pwkzyy@163.com>
2020-04-08 14:10:01 +0800

    xx

--- mock/mock.js
+++ mock/mock.js
@@ -5,4 +5,10 @@ import { baseUrl } from '@/common/vmeitime-http/interface.js'
 Mock.setup({
     timeout: '800-2000' // 延时响应300到1000毫秒之间的阈值
 })
-Mock.mock(baseUrl+'/banner/queryIndexBannerDtoPage.do', queryIndexBannerDtoPage) 
+
+const mockData = {
+	'/banner/queryIndexBannerDtoPage.do': Mock.mock(queryIndexBannerDtoPage),
+}
+
+
+export default mockData
